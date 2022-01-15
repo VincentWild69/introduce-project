27e28da9c816af9c6c7e5bf93e35cda7b7b1586d
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-01-15 23:49:01 +0200

    add contact section

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -4,7 +4,31 @@ import s from './Contacts.module.css';
 
 const Contacts = (props) => {
   return (
-    <div className={s.Contacts}>Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.Contacts Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum iure, et ullam exercitationem voluptatum dolores debitis quasi ducimus minus obcaecati expedita eum odio incidunt maiores sed praesentium consequuntur neque qui.</div>
+    <div className={s.contactsContainer}>
+      <h2 className={s.mainTitle}>My contacts</h2>
+      <ul className={s.contactList}>
+        <li className={s.contactItem}>
+          Telegram:&nbsp;
+          <a href="https://t.me/VincentGreen" className={s.contactLink} target="_blank" rel="noreferrer">@VincentGreen</a>
+        </li>
+        <li className={s.contactItem}>
+          Email:&nbsp;
+          <a href="mailto:alexeyk90@gmail.com" className={s.contactLink} target="_blank" rel="noreferrer">alexeyk90@gmail.com</a>
+        </li>
+        <li className={s.contactItem}>
+          Github:&nbsp;
+          <a href="https://github.com/VincentWild69" className={s.contactLink} target="_blank" rel="noreferrer">VincentWild69</a>
+          </li>
+        <li className={s.contactItem}>
+          Web:&nbsp;
+          <a href="http://vincent.pp.ua" className={s.contactLink} target="_blank" rel="noreferrer">vincent.pp.ua</a>
+          </li>
+        <li className={s.contactItem}>
+          CodeWars:&nbsp;
+          <a href="https://www.codewars.com/users/VincentWild69" className={s.contactLink} target="_blank" rel="noreferrer">My codewars</a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
