60273c489d4052937fcb9930787d3e6d7842a5f0
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-01-17 17:50:31 +0200

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import * as axios from 'axios';
 
+//использую соединение через прокси, чтобы можно было делать запросы в обход корс ошибки, настройки прокси в файле setupProxy.js
 
 export const quoteAPI = {
   getQuote: (lang) => {
