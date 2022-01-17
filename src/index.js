389c122d444a7ae2f3cc80f116e30a749be88686
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-01-17 16:37:34 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/introduce-project/'>
       <ThemeProvider>
         <App />
       </ThemeProvider>
