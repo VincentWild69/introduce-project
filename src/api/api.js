b6f6e305b1ce97bf6b819ac60a7cb44f1e495b21
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-01-17 16:49:16 +0200

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import * as axios from 'axios';
 
 export const quoteAPI = {
   getQuote: (lang) => {
-    return axios.get(`/api/1.0/?method=getQuote&format=json&lang=${lang}`).then(response => response.data)
+    return axios.get(`https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=${lang}`).then(response => response.data)
   },
 
 
