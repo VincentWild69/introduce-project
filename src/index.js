b639997d31f3206e2bd770b4d7b602eea317f38c
Alexey Kuzniecov <alexeyk90@gmail.com>
2022-02-22 17:40:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import MainLoader from './components/MainLoader/MainLoader';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/introduce-project/'>
+      <BrowserRouter basename='/introduce-project'>
       {/* <BrowserRouter> */}
         <ThemeProvider>
           <Suspense fallback={<MainLoader />}>
